8e47e9208c906aaf00ef205ed7d3f91ce3e0a0db
simon <5914123@qq.com>
2015-12-09 11:35:16 +0800

    update

--- posts/md.sh
+++ posts/md.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+output_file_name=${1%.*}
+output_file_full_name=${output_file_name}".html"
 markdown --html4tags $1 > temp
-cat header.html > converted.html
-cat temp >> converted.html
-cat footer.html >> converted.html
+cat header.html > $output_file_full_name 
+cat temp >> $output_file_full_name
+cat footer.html >> $output_file_full_name
 rm temp
 
