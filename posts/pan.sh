d2f9675a08a32c81d0932e75090d8d8ca489ab36
lixiang <5914123@qq.com>
2016-01-21 17:07:55 +0800

    add pandoc script

---
+++ posts/pan.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+output_file_name=${1%.*}
+output_file_full_name=${output_file_name}".html"
+pandoc $1 > temp
+cat header.html > $output_file_full_name 
+cat temp >> $output_file_full_name
+cat footer.html >> $output_file_full_name
+rm temp
+
